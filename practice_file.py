e1b29e46879c31afe2a14338e46b807b341652e0
behnaz <akhoondibehnaz@gmail.com>
2019-06-18 19:18:11 +0430

    male or female?

---
+++ practice_file.py
@@ -0,0 +1,7 @@
+gender = (input("enter your sex plz :(men \ female\n)"))
+if gender == "men" :
+    print("hello sir")
+elif gender == "female" :
+    print(" hello miss")
+else :
+    print("input is wrong, try again plz")
