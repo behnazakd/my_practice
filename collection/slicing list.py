aaac22ea69a2cfc7eba783fa41ee6dff022f8e61
behnaz <akhoondibehnaz@gmail.com>
2019-06-23 20:38:52 +0430

    methods and functions

---
+++ collection/slicing list.py
@@ -0,0 +1,11 @@
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+print(lst[2 : 12])
+print(lst[1 : -1])
+print(lst [::])
+print(lst[3 : -2])
+print(lst[0 : 8] + lst[13 :])
+print(lst[::2])
+print(lst[::-1])
+print(lst[::-2])
+print(lst[-2:])
+print(lst[:2])
