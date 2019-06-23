a0da6cea8785ce4e6c3e0a29ffd38e5aaf244d60
behnaz <akhoondibehnaz@gmail.com>
2019-06-23 20:28:26 +0430

    methods and functions

---
+++ calculate/gender.py
@@ -0,0 +1,7 @@
+gender = (input("enter your sex plz :(male \ female)"))
+if gender == "male" :
+    print("hello sir")
+elif gender == "female" :
+    print(" hello miss")
+else :
+    print("input is wrong, try again plz")
---
+++ collection/txt.py
@@ -0,0 +1,8 @@
+txt_one="hello"
+txt_tow = " "
+txt_three = "world"
+new_txt = txt_one+txt_tow+txt_three
+print(new_txt)
+print(len(txt_one))
+print(tuple(txt_one))
+print(*txt_one)
