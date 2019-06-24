2be30e8b5a640f0c747371406279afe349da1e95
behnaz <akhoondibehnaz@gmail.com>
2019-06-24 19:43:55 +0430

    calculate fibonacci

---
+++ collection/list_sorting.py
@@ -0,0 +1,15 @@
+list = ["s", "r", "f", "g", "n", "b", "z"]
+list.sort()
+print(list)
+list.sort(reverse = True)
+print(list)
+list_one = ['eat', 'ate', 'tea', 'sea', 'ease', 'see']
+list_one.sort()
+print(list_one)
+list_one.sort(reverse = True)
+print(list_one)
+list_tow = [43, 65, 20, 1, 79, 0.23, 102, 4]
+list_tow.sort()
+print(list_tow)
+list_tow.sort(reverse = True)
+print(list_tow)
